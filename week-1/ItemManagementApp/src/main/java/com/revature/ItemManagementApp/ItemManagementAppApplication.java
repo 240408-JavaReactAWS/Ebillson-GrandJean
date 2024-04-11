6d67caab03f1d7dc2b355621c30047bb5ab02159
BillGJ <gjebillson@gmail.com>
2024-04-11 16:55:43 -0400

    Base version for the Spring Boot Item Management App

---
+++ week-1/ItemManagementApp/src/main/java/com/revature/ItemManagementApp/ItemManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.revature.ItemManagementApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemManagementAppApplication.class, args);
+	}
+
+}
